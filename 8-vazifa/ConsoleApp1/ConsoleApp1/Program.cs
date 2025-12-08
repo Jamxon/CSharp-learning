e38bcc6d72a4e9b804b0e4f37f1e735868dfaa65
Jamxon <aliyevjamkhan499@gmail.com>
2025-12-08 11:27:07 +0500

    FS

--- 8-vazifa/ConsoleApp1/ConsoleApp1/Program.cs
+++ 8-vazifa/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ class  Program
             for (int j = 2; j < 10; j++)
             {
                 Console.WriteLine($"{i} * {j} = {i * j}");
+                
             }
         }
     }
--- 8-vazifa/ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ 8-vazifa/ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc182c954667ec5e0e71dc6ea5e3f6174894e18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
